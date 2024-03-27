4ddb6d0026cedc8fbc951fa5f28e5cedc629624b
saudkhanbpk <aqib@surfmobil.no>
2024-03-27 11:57:06 +0500

    first commit

--- backend/config/upload.js
+++ backend/config/upload.js
@@ -3,9 +3,9 @@ import { v2 as cloudinary } from 'cloudinary';
 
 //config cloudinary
 cloudinary.config({
-  cloud_name: 'drzsyyraw',
-  api_key: '817293329938495',
-  api_secret: 'oTGHjEamuZb-quJMFABHJCkhzp4'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 export default cloudinary;
